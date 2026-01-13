941fc174aa8da927347641b8c7e055ca74eed765
arvindyadav-codezilla <arvind.yadav@codezilla.io>
2026-01-13 15:58:54 +0530

    add changes

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  await app.listen(4000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 void bootstrap();
